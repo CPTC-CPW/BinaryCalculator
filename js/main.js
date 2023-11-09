e3810403abaa27e51366f426c3985f75b63deeb5
JoeProgrammer88 <JoeProgrammer88@users.noreply.github.com>
2023-11-09 11:21:36 -0800

    Created a function for decimal to binary conversion

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function decimalToBinary(decimal: number): string {
+function decimalToBinary(decimal) {
     let binary = "";
     while (decimal > 0) {
         binary = (decimal % 2) + binary;
@@ -6,12 +6,9 @@ function decimalToBinary(decimal: number): string {
     }
     return binary;
 }
-
 function binaryConversion(number) {
-    const binaryBits = [256,128, 64, 32, 16, 8, 4, 2, 1];
-
+    const binaryBits = [256, 128, 64, 32, 16, 8, 4, 2, 1];
     let resultString = "";
-
     while (number > 0) {
         const convertedBits = new Array(binaryBits.length).fill(0);
         for (let i = 0; i < binaryBits.length; i++) {
@@ -20,7 +17,6 @@ function binaryConversion(number) {
                 number -= binaryBits[i];
             }
         }
-
         for (let i = 0; i < convertedBits.length; i++) {
             resultString += convertedBits[i].toString();
         }
